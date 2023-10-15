cce9f5359d801c682061f52690faaec30778e2e1
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2023-10-15 15:46:53 -0300

    First Commit

---
+++ src/test/java/com/devdojospringboot/DevdojoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdojospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdojoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
