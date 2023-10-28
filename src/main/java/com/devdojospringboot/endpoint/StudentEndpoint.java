5e7f3aec9c88de45b2a8c11bfb9fbcc36f064631
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2023-10-27 22:07:18 -0300

    Get and Post 2/2

--- src/main/java/com/devdojospringboot/endpoint/StudentEndpoint.java
+++ src/main/java/com/devdojospringboot/endpoint/StudentEndpoint.java
@@ -10,8 +10,12 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.support.ServletContextAttributeFactoryBean;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devdojospringboot.error.CustomErrorType;
 import com.devdojospringboot.model.Student;
@@ -42,5 +46,12 @@ public class StudentEndpoint {
         }
         
         return ResponseEntity.ok(Student.studentList.get(index));
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        // return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(student.getId()).toUri()).build();
+        return ResponseEntity.ok(student);
     }
 }
